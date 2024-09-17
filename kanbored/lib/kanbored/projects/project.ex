0e429b22a56606e12cad74b4a7260924f237f360
Tiago Matos <tiagomatos3216@gmail.com>
2024-09-16 23:17:11 -0300

    Add function to delete project, introduces cache to get project data and refactor minor things.

--- kanbored/lib/kanbored/projects/project.ex
+++ kanbored/lib/kanbored/projects/project.ex
@@ -1,17 +1,20 @@
-defmodule Kanbored.Models.Project do
-  alias Kanbored.Models.{User, Board, UserProject}
+defmodule Kanbored.Project do
+  alias Kanbored.{User, Board, UserProject, TaskType}
   use Ecto.Schema
-  import Ecto.{Schema, Changeset}
+  import Ecto.Changeset
 
   @primary_key {:project_id, :binary_id, autogenerate: false}
   @foreign_key_type :binary_id
-
+    
   schema "projects" do
     field :project_name, :string
     field :description, :string
-    belongs_to :owner, User, foreign_key: :owner_id, references: :user_id
-    has_many :board, Board, foreign_key: :project_id
+    field :created_at, :naive_datetime
+    field :updated_at, :naive_datetime
 
+    belongs_to :owner, User, foreign_key: :owner_id, references: :user_id
+    has_many :boards, Board, foreign_key: :project_id
+    has_many :task_types, TaskType, foreign_key: :project_id
     many_to_many :users, User,
       join_through: UserProject,
       join_keys: [project_id: :project_id, user_id: :user_id]
@@ -19,7 +22,7 @@ defmodule Kanbored.Models.Project do
 
   def create_project_changeset(project, params \\ %{}) do
     project
-    |> cast(params, [:project_name, :description, :owner_id])
+    |> cast(params, [:project_name, :description, :owner_id, :updated_at])
     |> validate_required([:project_name, :owner_id])
   end
 end
--- kanbored/lib/kanbored/projects/projects.ex
+++ kanbored/lib/kanbored/projects/projects.ex
@@ -1,55 +1,98 @@
 defmodule Kanbored.Projects do
-  alias Kanbored.{Repo, Models.Project, Models.UserProject, Models.User}
+  alias Kanbored.{Repo, Project, Board, UserProject, User, Task, TaskType}
+  alias Ecto.Multi
+  import Ecto.Query, only: [from: 2]
 
   def new_project(attr \\ %{}) do
-    result =
-      %Project{}
-      |> Project.create_project_changeset(attr)
-      |> Repo.insert()
+    %Project{}
+    |> Project.create_project_changeset(attr)
+    |> Repo.insert()
+    |> case do
+      {:ok, project} -> {:ok, project}
+      {:error, result_error} -> result_error.errors |> Enum.map(fn {_, {error, _}} -> error end)
+    end
+  end
 
-    case result do
+  @spec get_project(%{project_id: String.t(), user_id: String.t()}) ::
+          {:ok, %Project{}} | {:error, :user_not_found_in_project | :project_not_found}
+  def get_project(%{project_id: project_id, user_id: user_id}) do
+    with true <- Repo.exists?(UserProject, user_id: user_id, project_id: project_id),
+         {:ok, project} <- get_project_from_cache(project_id) do
+      
+      {:ok, project}
+    else
+      false -> {:error, :user_not_found_in_project}
+      {:error, :project_not_found} -> {:error, :project_not_found}
+    end
+  end
+
+  @spec get_project_from_cache(String.t()) ::
+          {:ok, %Project{}} | {:error, :project_not_found}
+  defp get_project_from_cache(project_id) do
+    case Cachex.get(:kanbored_cache, project_id) do
+      {:ok, nil} -> get_project_from_repo(project_id)
       {:ok, project} ->
         {:ok, project}
-
-      {:error, result_error} ->
-        result_error.errors
-        |> Enum.map(fn {_, {error, _}} -> error end)
     end
   end
 
-  def delete_project(%{project_id: project_id, owner_id: owner_id}) do
+  @spec get_project_from_repo(String.t()) ::
+          {:ok, %Project{}} | {:error, :project_not_found}
+  defp get_project_from_repo(project_id) do
+    case Repo.get_by(Project, project_id: project_id) do
+      nil -> {:error, :project_not_found}
+      project ->
+        Cachex.put(:kanbored_cache, project_id, project)
+        {:ok, project}
+    end
+  end
+  
+  @spec remove_project(%{project_id: String.t(), owner_id: String.t()}) ::
+          {:ok, any()} | {:error, :project_not_found | :not_owner}
+  def remove_project(%{project_id: project_id, owner_id: owner_id}) do
     with project when not is_nil(project) <- Repo.get_by(Project, project_id: project_id),
          true <- project.owner_id == owner_id do
-      Repo.all(UserProject, project_id: project_id, owner_id: owner_id)
-      |> Enum.each(fn us -> Repo.delete(us) end)
-      Repo.delete(project)
+      Multi.new()
+      |> Multi.delete_all(:delete_user_projects, from(up in UserProject, where: up.project_id == ^project_id))
+      |> Multi.delete_all(
+        :delete_tasks,
+        from(t in Task,
+             join: b in Board, on: b.board_id == t.board_id,
+             join: p in Project, on: p.project_id == b.project_id,
+             where: p.project_id == ^project_id)
+      )
+      |> Multi.delete_all(:delete_boards, from(b in Board, where: b.project_id == ^project_id))
+      |> Multi.delete_all(:delete_task_types, from(ttp in TaskType, where: ttp.project_id == ^project_id))
+      |> Multi.delete(:delete_project, project)
+      |> Repo.transaction
     else
       nil -> {:error, :project_not_found}
-      :else -> {:error, :not_owner}
+      false -> {:error, :not_owner}
     end
   end
 
+  @spec add_user_to_project(%{project_id: String.t(), user_id: String.t()}) ::
+          {:ok, %UserProject{}} | list(String.t()) | {:error, :invalid}
   def add_user_to_project(%{project_id: project_id, user_id: user_id}) do
     with project when not is_nil(project) <- Repo.get_by(Project, project_id: project_id),
          user when not is_nil(user) <- Repo.get_by(User, user_id: user_id) do
-      insert_result =
-        %UserProject{}
-        |> UserProject.changeset(%{user_id: user_id, project_id: project_id})
-        |> Repo.insert()
-
-      case insert_result do
+      %UserProject{}
+      |> UserProject.changeset(%{user_id: user_id, project_id: project_id})
+      |> Repo.insert()
+      |> case do
         {:ok, result} ->
           {:ok, result}
 
         {:error, result_errors} ->
-          result_errors.errors
-          |> Enum.map(fn {_, {error, _}} -> error end)
+          result_errors.errors |> Enum.map(fn {_, {error, _}} -> error end)
       end
     else
-      error when error in [nil] -> [:error, :invalid]
+      error when error in [nil] -> {:error, :invalid}
     end
   end
 
+  @spec add_user_to_project(%{project_id: Strint.t(), users: list(Strint.t())}) ::
+          {:ok, list(any())} | {:error, :project_not_found}
   def add_user_to_project(%{project_id: project_id, users: user_list}) do
     list_of_added =
       case Repo.exists?(Project, project_id: project_id) do
@@ -58,7 +101,7 @@ defmodule Kanbored.Projects do
 
         true ->
           for user <- user_list do
-            if(Repo.exists?(User, user_id: user)) do
+            if Repo.exists?(User, user_id: user) do
               %UserProject{}
               |> UserProject.changeset(%{project_id: project_id, user_id: user})
               |> Repo.insert()
@@ -69,6 +112,8 @@ defmodule Kanbored.Projects do
     {:ok, list_of_added}
   end
 
+  @spec remove_user_from_project(%{project_id: String.t(), user_id: String.t()}) ::
+          {:ok, %UserProject{}} | {:error, :user_not_found_in_project}
   def remove_user_from_project(%{project_id: project_id, user_id: user_id}) do
     case Repo.get_by(UserProject, project_id: project_id, user_id: user_id) do
       nil ->

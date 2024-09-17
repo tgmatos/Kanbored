7f599a3108665eb41ee5ee6af2191436c533faf7
Tiago Matos <tiagomatos3216@gmail.com>
2024-09-16 23:31:07 -0300

    Add remove_board and refactor a few things

--- kanbored/lib/kanbored/boards/board.ex
+++ kanbored/lib/kanbored/boards/board.ex
@@ -1,14 +1,15 @@
-defmodule Kanbored.Models.Board do
-  alias Kanbored.Models.Project
+defmodule Kanbored.Board do
+  alias Kanbored.{Project, Task}
   use Ecto.Schema
   import Ecto.Changeset
 
   @primary_key {:board_id, :binary_id, autogenerate: false}
   @foreign_key_type :binary_id
-
+  
   schema "boards" do
     field :board_name, :string
     belongs_to :project, Project, foreign_key: :project_id, references: :project_id
+    has_many :tasks, Task, foreign_key: :board_id
   end
 
   def changeset(board, attr \\ %{}) do
--- kanbored/lib/kanbored/boards/boards.ex
+++ kanbored/lib/kanbored/boards/boards.ex
@@ -1,27 +1,45 @@
 defmodule Kanbored.Boards do
-  alias Kanbored.Models.{Board, Project}
+  alias Kanbored.{Board, Task, UserProject}
   alias Kanbored.Repo
+  alias Ecto.Multi
+  import Ecto.Query
 
   def new_board(attr \\ %{}) do
-    insert_result =
-      %Board{}
-      |> Board.changeset(attr)
-      |> Repo.insert()
+    %Board{}
+    |> Board.changeset(attr)
+    |> Repo.insert()
+    |> case do
+      {:ok, board} -> {:ok, board}
+      {:error, result_errors} -> result_errors.errors |> Enum.map(fn {_, {error, _}} -> error end)
+    end
+  end
+  
+  def remove_board(%{project_id: project_id, board_id: board_id, user_id: user_id}) do
+    Multi.new()
+    |> Multi.run(:check_user_in_project, fn repo, _changes ->
+      check_user_in_project(repo, %{project_id: project_id, user_id: user_id})
+    end)
+    |> Multi.run(:board, fn repo, _changes -> get_board(repo, board_id) end)
+    |> Multi.delete_all(:delete_tasks, &delete_tasks/1)
+    |> Multi.delete(:delete_board, &delete_board/1)
+    |> Repo.transaction
+  end
 
-    case insert_result do
-      {:ok, board} ->
-        {:ok, board}
-
-      {:error, result_errors} ->
-        result_errors.errors
-        |> Enum.map(fn {_, {error, _}} -> error end)
+  defp check_user_in_project(repo, %{project_id: project_id, user_id: user_id}) do
+    case repo.exists?(UserProject, project_id: project_id, user_id: user_id) do
+      false -> {:error, :user_not_found_on_project}
+      true -> {:ok, true}
     end
   end
 
-  def remove_board(board_id) do
-    case Repo.get_by(Board, board_id: board_id) do
-      nil -> {:error, :not_found}
-      board -> Repo.delete(board)
+  defp get_board(repo, board_id) do
+    case repo.get(Board, board_id) do
+      nil -> {:error, :board_not_found}
+      board -> {:ok, board}
     end
-  end
+  end  
+
+  defp delete_tasks(%{board: board}), do: from(t in Task, where: t.board_id == ^board.board_id)
+
+  defp delete_board(%{board: board}), do: board
 end
